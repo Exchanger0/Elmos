1a4ef0a6e1b61a595281d9a22b45803c323504b3
Eldar <nazaroveldar531@gmail.com>
2025-04-17 16:56:16 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from cx_Freeze import setup, Executable
 
+build_exe_option = {"include_files": [("files", "files")]}
+
 setup(
    name="Elmos",
    version="1.3",
-   description="Project",
+   description="Math calculator",
+   options={"build_exe": build_exe_option},
    executables=[Executable("start.py", base="gui")],
 )
